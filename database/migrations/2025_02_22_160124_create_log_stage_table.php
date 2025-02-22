4bd9f7e3db6dca86d6939bc5509c738f1ef16943
Habib Al Qadri <habibalqadri50@gmail.com>
2025-02-22 16:03:09 +0700

    add: log stage tabel migration

---
+++ database/migrations/2025_02_22_160124_create_log_stage_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('log_stage', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('lha_id')->nullable();
+            $table->tinyInteger("stage");
+            $table->text("keterangan");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('log_stage');
+    }
+};
