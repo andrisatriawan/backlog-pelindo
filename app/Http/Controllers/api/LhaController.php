02c6cbeb327dde28b2dcc4c83efb63a52a39888e
Habib Al Qadri <habibalqadri50@gmail.com>
2025-02-23 10:56:20 +0700

    add: update function cotnolle

--- app/Http/Controllers/api/LhaController.php
+++ app/Http/Controllers/api/LhaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\Lha;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class LhaController extends Controller
 {
@@ -67,11 +68,55 @@ class LhaController extends Controller
             return response()->json([
                 'status' => true,
                 'message' => 'Data berhasil disimpan!'
-            ]);
+            ], 201);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->errors()
+            ], 422);
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => false,
                 'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    // 🔹 Update Unit (UPDATE)
+    public function update(Request $request, $id)
+    {
+        try {
+            $unit = Lha::findOrFail($id);
+            if (!$unit || $unit->deleted == 1) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Unit tidak ditemukan'
+                ], 404);
+            }
+
+            $validated = $request->validate([
+                'no_lha' => 'required',
+                'judul' => 'required',
+                'periode' => 'required',
+                'deskripsi' => 'required'
+            ]);
+
+            $unit->update($validated);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Unit updated successfully',
+                'data' => $unit
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->errors()
+            ], 422);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage()
             ], 500);
         }
     }
