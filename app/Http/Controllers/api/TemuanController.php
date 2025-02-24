d1df834a0b3659f2c28425dedbc75f44bc7e7752
Habib Al Qadri <habibalqadri50@gmail.com>
2025-02-24 22:51:32 +0700

    add: show function show

---
+++ app/Http/Controllers/api/TemuanController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Temuan;
+use Illuminate\Http\Request;
+
+class TemuanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        try {
+            $length = 10;
+            if ($request->has('page_size')) {
+                $length = $request->page_size;
+            }
+            $temuan = Temuan::where('deleted', '!=', '1');
+
+            if ($request->has('keyword')) {
+                $keyword = strtolower($request->keyword);
+                $temuan->whereRaw('LOWER(nama) LIKE ?', ["%{$keyword}%"]);
+            }
+
+            $temuan->orderBy('id', 'ASC');
+            $data = $temuan->paginate($length);
+
+            return response()->json([
+                'status' => true,
+                'data' => $data->items(),
+                'pagination' => [
+                    'current_page' => $data->currentPage(),
+                    'total' => $data->total(),
+                    'per_page' => $data->perPage(),
+                    'last_page' => $data->lastPage(),
+                    'next_page_url' => $data->nextPageUrl(),
+                    'prev_page_url' => $data->previousPageUrl(),
+                ]
+            ]);
+        } catch (\Throwable $e) {
+            $code = 500;
+            if ($e->getCode()) {
+                $code = $e->getCode();
+            }
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage()
+            ], $code);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        try {
+            $temuan = Temuan::find($id);
+            if (!$temuan || $temuan->deleted == 1) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Temuan tidak ditemukan'
+                ], 404);
+            }
+
+            return response()->json([
+                'status' => true,
+                'data' => $temuan
+            ]);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
