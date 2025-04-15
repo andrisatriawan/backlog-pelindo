9a69238ed7e9b501c007571979553c69a34bd67e
Andri Satriawan <satria.citieng@gmail.com>
2025-04-15 21:09:18 +0700

    feat: penyesuaian dashboard pic dan pj

--- app/Http/Controllers/api/StatistikController.php
+++ app/Http/Controllers/api/StatistikController.php
@@ -13,7 +13,21 @@ class StatistikController extends Controller
 {
     public function logStage(Request $request)
     {
+        $roleName = auth()->user()->roles->pluck('name')->toArray();
+
         $query = LogStage::where('model_type', Temuan::class);
+
+        if (in_array('pic', $roleName)) {
+            $temuanIds = Temuan::where('divisi_id', auth()->user()->divisi_id)->pluck('id');
+
+            $query->whereIn('model_id', $temuanIds);
+        }
+
+        if (in_array('penanggungjawab', $roleName)) {
+            $temuanIds = Temuan::where('unit_id', auth()->user()->unit_id)->pluck('id');
+
+            $query->whereIn('model_id', $temuanIds);
+        }
 
         if ($request->has('lha_id')) {
             $temuanIds = Temuan::where('lha_id', $request->lha_id)->pluck('id');
@@ -46,11 +60,33 @@ class StatistikController extends Controller
 
     public function dashboardSummary(Request $request)
     {
+        $roleName = auth()->user()->roles->pluck('name')->toArray();
+
         $totalLha = Lha::where('deleted', '!=', '1')->count();
         $totalTemuan = Temuan::where('deleted', '!=', '1')->count();
         $temuanSelesai = Temuan::where('deleted', '!=', '1')->where('status', '3')->count();
         $temuanAktif = Temuan::where('deleted', '!=', '1')->whereIn('status', ['1', '4'])->count();
         $temuanAuditor = Temuan::where('deleted', '!=', '1')->where('status', '2')->count();
+
+        if (in_array('pic', $roleName)) {
+            $totalLha = Lha::where('deleted', '!=', '1')->whereHas('temuan', function ($query) {
+                $query->where('divisi_id', auth()->user()->divisi_id);
+            })->count();
+            $totalTemuan = Temuan::where('deleted', '!=', '1')->where('divisi_id', auth()->user()->divisi_id)->count();
+            $temuanSelesai = Temuan::where('deleted', '!=', '1')->where('divisi_id', auth()->user()->divisi_id)->where('status', '3')->count();
+            $temuanAktif = Temuan::where('deleted', '!=', '1')->where('divisi_id', auth()->user()->divisi_id)->whereIn('status', ['1', '4'])->count();
+            $temuanAuditor = Temuan::where('deleted', '!=', '1')->where('divisi_id', auth()->user()->divisi_id)->where('status', '2')->count();
+        }
+
+        if (in_array('penanggungjawab', $roleName)) {
+            $totalLha = Lha::where('deleted', '!=', '1')->whereHas('temuan', function ($query) {
+                $query->where('unit_id', auth()->user()->unit_id);
+            })->count();
+            $totalTemuan = Temuan::where('deleted', '!=', '1')->where('unit_id', auth()->user()->unit_id)->count();
+            $temuanSelesai = Temuan::where('deleted', '!=', '1')->where('unit_id', auth()->user()->unit_id)->where('status', '3')->count();
+            $temuanAktif = Temuan::where('deleted', '!=', '1')->where('unit_id', auth()->user()->unit_id)->whereIn('status', ['1', '4'])->count();
+            $temuanAuditor = Temuan::where('deleted', '!=', '1')->where('unit_id', auth()->user()->unit_id)->where('status', '2')->count();
+        }
 
         if ($request->has('lha_id')) {
             $lha = Lha::find($request->lha_id);
